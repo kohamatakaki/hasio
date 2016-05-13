775b3151cbc378e4c7590ce175927f347ee01ef8
小浜天紀 <kohamatakaki@obamaakihito-no-macbook-pro.local>
2016-05-13 19:40:00 +0900

    Initial Commit

---
+++ hasioTests/hasioTests.swift
@@ -0,0 +1,36 @@
+//
+//  hasioTests.swift
+//  hasioTests
+//
+//  Created by 小浜天紀 on 2016/05/13.
+//  Copyright © 2016年 小浜天紀. All rights reserved.
+//
+
+import XCTest
+@testable import hasio
+
+class hasioTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
